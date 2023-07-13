a339319f8b505eca2c086124e12fea9e91ddf98a
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-13 16:04:41 +0200

    Reestructure styles folders

--- client/src/components/baseComponents/Avatar/Avatar.jsx
+++ client/src/components/baseComponents/Avatar/Avatar.jsx
@@ -1,7 +1,17 @@
-const Avatar = ({src, alt}) => {
+import PropTypes from 'prop-types';
+
+const Avatar = ({
+  src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVzZXIlMjBhdmF0YXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60', 
+  alt='user avatar'
+}) => {
   return (
-    <img src={src} alt={alt} />
+    <img className='userAvatar' src={src} alt={alt} />
   );
 };
+
+Avatar.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+}
 
 export default Avatar;
