8702004fd8dddd196b31c928c365eb46caf41570
Mariangelica Rodriguez <gromarant@gmail.com>
2023-07-14 22:47:33 +0200

    Add user home structure and style

--- client/src/components/baseComponents/Card/Card.jsx
+++ client/src/components/baseComponents/Card/Card.jsx
@@ -1,45 +1,47 @@
-// import { Card as CardNextUi, Grid, Text, Spacer, Button } from "@nextui-org/react";
+import { Card as CardNextUi, Grid, Text, Spacer, Button } from "@nextui-org/react";
 import PropTypes from 'prop-types';
 
 
 const Card = ({content}) => {
 
-  // const {title, image, alt } = content;
+ 
   return (
     <>
-      {/* <CardNextUi css={{ mw: "400px" }}>
-      <Grid.Container css={{ pl: "$6" }}>
-        <Grid xs={12}>
-          <Text h3 css={{ lineHeight: "$xs" }}>
-            {pokemonDetails.name}
-          </Text>
-        </Grid>
-        <Card.Image
-            src={pokemonDetails?.sprites?.other['official-artwork']?.front_default}
-            objectFit="cover"
-            width="fit-content"
-            alt={pokemonDetails.name}
-          />
-        <Grid xs={12}>
-          <Text h4 css={{ lineHeight: "$xs" }}>Peso</Text>
-          <Spacer x={1} />
-          <Text css={{ color: "$accents8" }}>{`${pokemonDetails.weight / 10} kg`}</Text>
-          <Spacer x={2} />
-          <Text h4 css={{ lineHeight: "$xs" }}>Altura</Text>
-          <Spacer x={1} />
-          <Text css={{ color: "$accents8" }}>{`${pokemonDetails.height / 10} mts`}</Text>
-        </Grid>
-        <Spacer y={1} />
-        <Card.Divider />
-      </Grid.Container>
-      <Grid.Container gap={1}>
-      {pokemonDetails.types && pokemonDetails.types?.map(type => (<Grid key={uuidv4()}>
-          <Button flat color="secondary" auto>{type.type.name}</Button>
-        </Grid> 
-        ))
-      }
-      </Grid.Container>
-      </CardNextUi> */}
+      <article className='card'>
+        <CardNextUi className='card_F_layer'>
+          <Grid.Container>
+            <Grid xs={12}>
+              <h4>
+                {content.name}
+              </h4>
+            </Grid>
+            {/* <Card.Image
+                src={pokemonDetails?.sprites?.other['official-artwork']?.front_default}
+                objectFit="cover"
+                width="fit-content"
+                alt={pokemonDetails.name}
+              />
+            <Grid xs={12}>
+              <Text h4 css={{ lineHeight: "$xs" }}>Peso</Text>
+              <Spacer x={1} />
+              <Text css={{ color: "$accents8" }}>{`${pokemonDetails.weight / 10} kg`}</Text>
+              <Spacer x={2} />
+              <Text h4 css={{ lineHeight: "$xs" }}>Altura</Text>
+              <Spacer x={1} />
+              <Text css={{ color: "$accents8" }}>{`${pokemonDetails.height / 10} mts`}</Text>
+            </Grid>
+            <Spacer y={1} />
+            <Card.Divider />
+          </Grid.Container>
+          <Grid.Container gap={1}>
+          {pokemonDetails.types && pokemonDetails.types?.map(type => (<Grid key={uuidv4()}>
+              <Button flat color="secondary" auto>{type.type.name}</Button>
+            </Grid> 
+            ))
+          } */}
+          </Grid.Container>
+        </CardNextUi>
+      </article>
     </>
   );
 };
--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
-// import Avatar from '../../baseComponents/Avatar/Avatar';
+import Avatar from '../../baseComponents/Avatar/Avatar';
+import Card from '../../baseComponents/Card/Card';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [lists, setlists] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/lists')
+      .then(response => response.json())
+      .then(data => setlists(data))
+      
+  }, [])
+
+
   return (
     <>
-      {/* <Avatar className='userAvatar' />  */}
+      <Avatar className='userAvatar' /> 
+      <p>LuisR@gmail.com</p>
+      <span>Icon crear lista</span>
+      <section className='userList_section'>
+        {lists && lists?.map(list => <Card content={{"name": `${list.name}`}} key={`cBcR${list.name}`} />)}
+      </section>
     </>
-  );
+  )
 };
-
 export default Home;
